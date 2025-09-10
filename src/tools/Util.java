737f3ec027039ed2aa5de975a1f2b0de7d0a42c4
natgreis <nathalia.reis@estudante.ifms.edu.br>
2025-09-10 12:31:37 -0400

    tarefa de fazer as coisas no tools

--- src/tools/Util.java
+++ src/tools/Util.java
@@ -4,7 +4,11 @@
  */
 package tools;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
@@ -16,49 +20,68 @@ import javax.swing.JTextField;
  * @author u1845853
  */
 public class Util {
-    public static void habilitar(boolean valor, JComponent ... componentes) {
+
+    public static void habilitar(boolean valor, JComponent... componentes) {
         for (int i = 0; i < componentes.length; i++) {
             componentes[i].setEnabled(valor);
-            
+
         }
     }
-    public static void limpar(JComponent ... componentes){
-        for (int i = 0; i < componentes.length; i++){
-            if (componentes[i] instanceof JTextField){
-           ((JTextField) componentes[i]).setText("");      
-            }
-            else if (componentes[i] instanceof JComboBox){
-           ((JComboBox) componentes[i]).setSelectedIndex(-1);      
-            }
-            else if (componentes[i] instanceof JCheckBox) {
-                ((JCheckBox) componentes[i]).setSelected(false); 
+
+    public static void limpar(JComponent... componentes) {
+        for (int i = 0; i < componentes.length; i++) {
+            if (componentes[i] instanceof JTextField) {
+                ((JTextField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JComboBox) {
+                ((JComboBox) componentes[i]).setSelectedIndex(-1);
+            } else if (componentes[i] instanceof JCheckBox) {
+                ((JCheckBox) componentes[i]).setSelected(false);
             }
         }
     }
-    public static void mensagem(String cad){
+
+    public static void mensagem(String cad) {
         JOptionPane.showMessageDialog(null, cad);
     }
-    
-    public static boolean pergunta(String cad){
+
+    public static boolean pergunta(String cad) {
         JOptionPane.showConfirmDialog(null, cad);
         return true;
     }
-    public static int strToInt (String num) {
+
+    public static int strToInt(String num) {
         return Integer.parseInt(num);
     }
+
     public static String intToStr(int num) {
         return String.valueOf(num);
     }
-    public static double strToDouble (String num){
-        return 0.0;
+
+    public static double strToDouble(String num) {
+        try {
+            return Double.parseDouble(num);
+        } catch (NumberFormatException e) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, e);
+            return 0.0;
+        }
     }
-    public static String doubleToStr (double num){
-        return "";
+
+    public static String doubleToStr(double num) {
+        return String.valueOf(num);
     }
-    public static Date strToDate (String data){
+
+    public static Date strToDate(String data) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            return sdf.parse(data);
+        } catch (ParseException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return null;
     }
-    public static String dateToStr(Date data){
-        return "";
+
+    public static String dateToStr(Date data) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(data);
     }
 }
